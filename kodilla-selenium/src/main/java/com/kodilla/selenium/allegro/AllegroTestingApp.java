8b0ffb349a3431021fe6eca316843a6140faf2b7
Jarosław Buła <jar3000ek@gmail.com>
2020-05-08 08:49:15 +0200

    Selenium allegro

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,25 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AllegroTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.allegro.pl");
+        while (!driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/button")).isDisplayed()) {
+        }
+        driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/button")).click();
+
+        WebElement setCombo = driver.findElement(By.xpath("//*[@class=\"_r65gb _1fwkl\"]/select"));
+        Select selectElectronic = new Select(setCombo);
+        selectElectronic.selectByIndex(3);
+
+        WebElement setTextField = driver.findElement(By.xpath("//*[@data-box-name=\"allegro.metrumHeader.search\"]/div/form/input"));
+        setTextField.sendKeys("Mavic mini");
+    }
+}
